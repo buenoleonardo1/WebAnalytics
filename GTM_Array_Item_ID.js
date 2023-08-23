5d7a8ddad2b2eac59412b8a7859469ebc588f7b0
buenoleonardo1 <82121400+buenoleonardo1@users.noreply.github.com>
2023-08-23 11:24:18 -0300

    Create GTM_Array_Item_ID.js
    Google Tag Manager: Capturando Variáveis Específicas do Array "Items" do Data Layer
    
    
    
    Algumas plataformas de publicidade, como Meta Ads e TikTok Ads, pedem um parâmetro chamado "content_id" ou "content_ids", que basicamente requer um array com os ID's dos produtos (SKU's). Ou seja, na compra da imagem, o valor do "content_id" seria ["18","56"].
    
    
    
    Mas como capturar essa informação? Se só tivessemos um produto, poderíamos capturar seu ID usando a estrutura ecommerce.items.0.item_id. Mas como capturar quando há mais de um produto?
    
    
    
    Para isso, é necessário criar um javascript personalizado que faz o famoso loop "for ... in" para percorrer os índices (ou chaves) da matriz "items" e aí sim capturar os id's que precisamos, jogando apenas eles em um array.

---
+++ GTM_Array_Item_ID.js
@@ -0,0 +1,21 @@
+function getItemIds() {
+  // Definindo uma função chamada getItemIds.
+  
+  var items = {{ecommerce_items}}; //substitua pelo nome da sua variável
+  // Obtém a variável "ecommerce_items" do ambiente do Google Tag Manager e armazena na variável "items".
+  // Certifique-se de que a variável "ecommerce_items" esteja corretamente configurada no GTM para capturar a matriz de itens.
+  
+  var itemIds = [];
+  // Inicializa um array vazio chamado "itemIds" para armazenar os IDs dos itens.
+
+  for (var i in items) {
+    // Inicia um loop "for ... in" para percorrer os índices (ou chaves) da matriz "items".
+
+    itemIds.push(items[i]['item_id']);
+    // Para cada índice, obtém o valor da propriedade 'item_id' do objeto atual na matriz "items".
+    // E adiciona esse valor ao array "itemIds".
+  }
+  
+  return itemIds;
+  // Retorna o array "itemIds" que contém os IDs dos itens.
+}
